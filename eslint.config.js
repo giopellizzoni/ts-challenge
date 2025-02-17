b37ed6d31328ac146e191b0e7c66b2e0e45af5ef
Giovanni Pellizzoni <pellizzonicode@gmail.com>
2025-02-17 13:55:27 +0100

    Disable React JSX scope rules in ESLint and fix typo in Sessions page description

--- eslint.config.js
+++ eslint.config.js
@@ -14,6 +14,8 @@ export default [
     rules: {
       "no-unused-vars": "warn",
       "no-undef": "warn",
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off",
     },
   },
   {
--- src/pages/Sessions.tsx
+++ src/pages/Sessions.tsx
@@ -6,7 +6,7 @@ export default function SessionsPage() {
       <header>
         <h2>Available mentoring sessions</h2>
         <p>
-          From an one-on-one introduction to React's basics all the way up to a
+          From an one-on-one introduction to React&apos;s basics all the way up to a
           deep dive into state mechanics - we got just the right session for
           you!
         </p>
